d5b978d024a742a1c21d84db450bb20bb59dc778
Jason <jhphan1@gmail.com>
2022-03-10 19:02:19 -0800

    Import loadPage() into index.js

--- src/home.js
+++ src/home.js
@@ -1,17 +1,17 @@
-function loadPage() {
+const loadPage = () => {
     const header = document.createElement("div");
     header.classList.add("header");
     const logo = document.createElement("div");
     logo.classList.add("logo");
-    logo.textContent = "Chicken<br>& Broccoli";
+    logo.innerHTML = "Chicken<br>& Broccoli";
     const nav = document.createElement("ul");
     nav.classList.add("nav");
     const li1 = document.createElement("li");
     li1.textContent = "HOME";
     const li2 = document.createElement("li");
-    li2.textContent = "HOME";
+    li2.textContent = "C&B MENU";
     const li3 = document.createElement("li");
-    li3.textContent = "HOME";
+    li3.textContent = "CONTACT & LOCATIONS";
 
     header.appendChild(logo);
     header.appendChild(nav);
@@ -21,3 +21,5 @@ function loadPage() {
 
     return header;
 }
+
+export { loadPage };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import { loadPage } from "./home.js";
 
 
 const content = document.querySelector("#content");
